8eb5203b832be533e435c5deb49cc0870af50187
Justin Lange <well1912@gmail.com>
2016-03-29 09:11:07 -0700

    Combine js-test.js into map.js

--- js/map.js
+++ js/map.js
@@ -18,10 +18,13 @@ function init() {
 
   var mapElement = document.getElementById('map');
   map = new google.maps.Map(mapElement, mapOptions);
-  markCurrentLocation();
+  markCurrentLocation(function () {
+    sortByDistance(pos.lat, pos.lng, snapData);
+    addAllMarkers();
+  });
 }
 
-function markCurrentLocation() {
+function markCurrentLocation(cb) {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
       pos = {
@@ -34,12 +37,45 @@ function markCurrentLocation() {
         animation: google.maps.Animation.DROP
       });
       posMarker.setMap(map);
+      cb();
     }, function() {
       handleLocationError(true, infoWindow, map.getCenter());
     });
   } else {
     handleLocationError(false, infoWindow, map.getCenter());
   }
+}
+
+function sortByDistance(myLatitude, myLongitude, world) {
+  var distances = [];
+  for (var i = 0; i < world.length; i++) {
+    var place = world[i];
+    var distance = Math.sqrt(Math.pow(myLatitude - place.Latitude, 2) + Math.pow(myLongitude - place.Longitude, 2)); // Uses Euclidean distance
+    distances.push({distance: distance, place: place});
+  }
+  // Return the distances, sorted
+  markers = distances.sort(function(a, b) {
+    return a.distance - b.distance; // Switch the order of this subtraction to sort the other way
+  })
+  .slice(0, 10); // Gets the first ten places, according to their distance
+}
+
+function addAllMarkers() {
+  markers.forEach(function(snapLocation) {
+    var marker = new google.maps.Marker({
+      position: {
+        lat: snapLocation.place.Latitude,
+        lng: snapLocation.place.Longitude
+      },
+      clickable: true,
+      map: map,
+      animation: google.maps.Animation.DROP
+    });
+    marker.setMap(map);
+    // markers.push(marker);
+    console.log('Oh SNAP Latitude: ' + snapLocation.place.Latitude);
+    console.log('Oh SNAP Longitude: ' + snapLocation.place.Longitude);
+  });
 }
 
 function makeMapOptions() {
