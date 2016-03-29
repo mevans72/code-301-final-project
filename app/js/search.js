12a23e5c763f2cce244d47fea4f13a4f2af23b53
Mars <marshallmiller92@gmail.com>
2016-03-29 14:16:41 -0700

    autocomplete functionality sorta done

--- app/js/search.js
+++ app/js/search.js
@@ -2,6 +2,25 @@ google.maps.event.addDomListener(window, 'load', function(){
   $(".search-bar input").autocomplete({
     source: snapData.all.map(function(s){
       return s.Store_Name
-    })
-  });
+    }),
+    // focus: function( event, ui ) {
+      // console.log(ui);
+      //  $( "#project" ).val( ui.item.label );
+      // return false;
+    // },
+    // select: function( event, ui ) {
+      // console.log('select' + ui);
+      //  $( "#project" ).val( ui.item.label );
+      //  $( "#project-id" ).val( ui.item.value );
+      //  $( "#project-description" ).html( ui.item.desc );
+      //  $( "#project-icon" ).attr( "src", "images/" + ui.item.icon );
+
+      // return false;
+    // }
+  })
+  .autocomplete( "instance" )._renderItem = function( ul, item ) {
+    return $( "<li>" )
+    .append( "<a>" + item.label + "</a>" )
+    .appendTo( ul );
+  };
 });
