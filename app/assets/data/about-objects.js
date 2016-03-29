999c2f0e25def3658af90a0ed9c621bdb8b36e21
mevans72 <michaleevans72@gmail.com>
2016-03-29 13:16:04 -0700

    Pushing some cool stuff like app and firebase.json to dev...

---
+++ app/assets/data/about-objects.js
@@ -0,0 +1,18 @@
+var aboutUsData = [
+  {
+    name: 'Marshall Miller'
+    body: '<p></p>'
+  },
+  {
+    name: 'Mike Evans'
+    body: '<p></p>'
+  },
+  {
+    name: 'Marcus Su'
+    body: '<p></p>'
+  },
+  {
+    name: 'Justin Lange'
+    body: '<p></p>'
+  }
+]
