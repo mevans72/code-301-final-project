f20da748190d55c4216cd72a680cdbaa237f17d2
Justin Lange <well1912@gmail.com>
2016-03-31 13:37:21 -0700

    Implement basic firebase retrieval

--- app/js/review-model.js
+++ app/js/review-model.js
@@ -1,24 +1,36 @@
 (function (module) {
-  module.reviews = [];
+  var cb, firebaseUrl = 'https://code-301-final-project-2.firebaseio.com/';
+
+  var reviews = [];
 
   function getReviews(id) {
     // TODO: replace with call to firebase
-    return [{
-      name: 'a',
-      body: "it's ok",
-      rating: 3
-    }, {
-      name: 'b',
-      body: "it's good",
-      rating: 5
-    }, {
-      name: 'c',
-      body: "it's bad",
-      rating: 5
-    }];
+    return reviews;
   }
 
+  function newRef(cb) {
+    var ref = new Firebase(firebaseUrl);
+
+    ref.on('value', function (snapshot) {
+      var val = snapshot.val(),
+          values = [];
+      for (k in val) {
+        values.push(val[k]);
+      }
+      cb(values);
+    });
+
+    return function (id, review) {
+      ref.child(id).push(review);
+    };
+  }
+
+  var pushReview =  newRef(function (newReviews) {
+    reviews = newReviews;
+  });
+
   module.reviews = {
-    get: getReviews
+    get: getReviews,
+    push: pushReview
   };
 })(window);
