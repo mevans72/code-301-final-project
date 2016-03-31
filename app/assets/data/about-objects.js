c8e12d3632e7e1fb57c23e0ed58c67aa9ab671ae
Mars <marshallmiller92@gmail.com>
2016-03-31 11:09:46 -0700

    fixing bugs

--- app/assets/data/about-objects.js
+++ app/assets/data/about-objects.js
@@ -1,17 +1,21 @@
 var aboutUsData = [
   {
+    img:
     name: 'Marshall Miller'
     body: '<p></p>'
   },
   {
+    img:
     name: 'Mike Evans'
     body: '<p></p>'
   },
   {
+    img:
     name: 'Marcus Su'
     body: '<p></p>'
   },
   {
+    img:
     name: 'Justin Lange'
     body: '<p></p>'
   }
