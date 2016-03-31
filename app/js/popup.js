35d8db401e51d3a856009052f302a73115ae5446
MarcusSu <marcusssu@gmail.com>
2016-03-31 15:47:43 -0700

    fix popup

--- app/js/popup.js
+++ app/js/popup.js
@@ -1,46 +1,61 @@
 $(document).ready(function() {
+
   $('.resources-button').click(function(e) {
     $('#resources').fadeIn();
   });
-  $('#resources').click(function(e){
-    e.stopPropagation();
-  });
-  $(document).click(function() {
+
+  $('#resources').click(function(e) {
     $('#resources').fadeOut();
   });
-});
-
-
 
   $('.purpose-button').click(function(e) {
-    console.log('close');
-    e.stopPropagation();
-    $('#purposeus').fadeOut();
-  });
-  $(document).click(function() {
-    $('#purposeus').fadeOut();
-  });
-  $('.purpose-button').click(function(e) {
-    e.preventDefault();
     $('#purposeus').fadeIn();
   });
 
-
+  $('#purpose').click(function(e) {
+    $('#purposeus').fadeOut();
+  });
 
   $('.help-button').click(function(e) {
-    console.log('close');
-    e.stopPropagation();
-    $('#help').fadeOut();
-  });
-  $(document).click(function() {
-    $('#help').fadeOut();
-  });
-  $('.help-button').click(function(e) {
-    e.preventDefault();
     $('#help').fadeIn();
-
-    $('.bxslider').bxSlider({
-      mode: 'fade',
-      captions: false
-    });
   });
+
+  $('#help').click(function(e) {
+    $('#help').fadeOut();
+  });
+
+
+
+  // $('.purpose-button').click(function(e) {
+  //   console.log('close');
+  //   e.stopPropagation();
+  //   $('#purposeus').fadeOut();
+  // });
+  // $(document).click(function() {
+  //   $('#purposeus').fadeOut();
+  // });
+  // $('.purpose-button').click(function(e) {
+  //   e.preventDefault();
+  //   $('#purposeus').fadeIn();
+  // });
+  //
+  //
+  //
+  // $('.help-button').click(function(e) {
+  //   console.log('close');
+  //   e.stopPropagation();
+  //   $('#help').fadeOut();
+  // });
+  // $(document).click(function() {
+  //   $('#help').fadeOut();
+  // });
+  // $('.help-button').click(function(e) {
+  //   e.preventDefault();
+  //   $('#help').fadeIn();
+  //
+  //   $('.bxslider').bxSlider({
+  //     mode: 'fade',
+  //     captions: false
+  //   });
+  // });
+});
