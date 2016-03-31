128fbda78f8ca74de5660f94e193f51c05b616b2
mevans72 <michaleevans72@gmail.com>
2016-03-31 11:58:20 -0700

    Working with J-Dog on fixin a merge conflict

--- app/js/routes.js
+++ app/js/routes.js
@@ -3,6 +3,7 @@ page('/', controller.base, controller.app);
 page('/review/:id', controller.base, controller.review);
 page('/new-review/:id', controller.base, controller.newReview);
 
+page('/search', controller.base, controller.search);
 page('/review-back', controller.base, controller.reviewBack);
 page('/about', controller.base, controller.about);
 page('/help', controller.base, controller.help);
