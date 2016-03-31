40594c3bbce809a003bff29a72552d391ea6ec2c
Justin Lange <well1912@gmail.com>
2016-03-30 22:22:44 -0700

    Add context saving to controllers

--- app/js/controller.js
+++ app/js/controller.js
@@ -8,7 +8,7 @@
     next();
   }
 
-  controller.search = function() {
+  controller.search = function(context) {
     // e.preventDefault();
     if ($('#slide-bar').css('right') == '0px') {
       $('#slide-bar').css('right', '-300px');
@@ -18,50 +18,57 @@
       $('#slide-bar').css('right', '0px');
       $(this).find('a').attr("href", "/");
     }
+    context.save();
   };
 
-  controller.review = function() {
+  controller.review = function(context) {
     if ($('#review-bar').css('right') === '-300px') {
       $('#review-bar').css('right', '0px');
     } else {
       $('#review-bar').css('right', '0px');
     }
+    context.save();
   };
-  controller.reviewBack = function() {
+
+  controller.reviewBack = function(context) {
     if ($('#review-bar').css('right') == '0px') {
       $('#review-bar').css('right', '-300px');
     } else {
       $('#review-bar').css('right', '0px');
     }
+    context.save();
   };
 
-  controller.about = function() {
+  controller.about = function(context) {
     console.log("About is great!");
     $("#fancy-about").fancybox({
       openEffect: 'none',
       closeEffect: 'none'
     });
+    context.save();
   };
 
-  controller.help = function() {
+  controller.help = function(context) {
     console.log("Help is great!");
     if ($('#fancy-help').css('display') !== 'visible') {
       $('#ancy-help').show();
     } else {
       $('#ancy-help').hide();
     }
+    context.save();
   };
 
-  controller.resources = function() {
+  controller.resources = function(context) {
     console.log("Help is great!");
     if ($('#fancy-resources').css('display') !== 'visible') {
       $('#ancy-resources').show();
     } else {
       $('#ancy-resources').hide();
     }
+    context.save();
   };
 
-  controller.newReview = function () {
+  controller.newReview = function (context) {
     $('#write-review').css('display', 'block');
   }
 
