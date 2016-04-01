edcec33b426a590cba988acc8c2de3ee74bcf815
mevans72 <michaleevans72@gmail.com>
2016-03-31 17:30:37 -0700

    Lots of cleanup and beautifying...

--- app/js/popup.js
+++ app/js/popup.js
@@ -23,39 +23,4 @@ $(document).ready(function() {
   $('#help').click(function(e) {
     $('#help').fadeOut();
   });
-
-
-
-  // $('.purpose-button').click(function(e) {
-  //   console.log('close');
-  //   e.stopPropagation();
-  //   $('#purposeus').fadeOut();
-  // });
-  // $(document).click(function() {
-  //   $('#purposeus').fadeOut();
-  // });
-  // $('.purpose-button').click(function(e) {
-  //   e.preventDefault();
-  //   $('#purposeus').fadeIn();
-  // });
-  //
-  //
-  //
-  // $('.help-button').click(function(e) {
-  //   console.log('close');
-  //   e.stopPropagation();
-  //   $('#help').fadeOut();
-  // });
-  // $(document).click(function() {
-  //   $('#help').fadeOut();
-  // });
-  // $('.help-button').click(function(e) {
-  //   e.preventDefault();
-  //   $('#help').fadeIn();
-  //
-  //   $('.bxslider').bxSlider({
-  //     mode: 'fade',
-  //     captions: false
-  //   });
-  // });
 });
